1f94a075926e94ed61694c0e2b2f6b8b6367e341
huzaifa250 <huz.dark1@gmail.com>
2024-10-22 10:33:05 +0000

    add 4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,19 @@
+-- Creates a trigger that decreases the quantity
+-- of an item after adding a new order.
+DROP TRIGGER IF EXISTS reduce_quantity;
+DELIMITER
+CREATE TRIGGER reduce_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+-- Ensure that there's enough stock before reducing the quantity
+IF (SELECT quantity FROM items WHERE name = NEW.item_name) >= NEW.number THEN
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+ELSE
+	SIGNAL SQLSTATE '45000'
+	SET MESSAGE_TEXT = 'Insufficient stock for item: ' + NEW.item_name;
+END IF
+END
+DELIMITER;
