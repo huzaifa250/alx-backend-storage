e7dc8d710b471ac4d55d7084ca7f61d54b8e76dd
huzaifa250 <huz.dark1@gmail.com>
2024-10-23 13:29:40 -0700

    edit task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python3
-'''Task 11's module.
-'''
+'''Module to find topic'''
 
 
 def schools_by_topic(mongo_collection, topic):
-    '''Returns the list of school having a specific topic.
-    '''
-    topic_filter = {
-        'topics': {
-            '$elemMatch': {
-                '$eq': topic,
-            },
-        },
-    }
-    return [doc for doc in mongo_collection.find(topic_filter)]
+    '''Returns the list of school having a specific topic.'''
+    return mongo_collection.find({"topics": topic})
